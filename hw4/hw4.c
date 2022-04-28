3d9eb5233d0cafe02bff32a8cf5a9bc4ceb6257e
B1029028 <1z2x3c4vqaws@gmail.com>
2022-04-28 16:35:25 +0800

    hw4

---
+++ hw4/hw4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int* my_calloc(int n, int size) {
+    for () {
+        for () {
+            if () {
+
+            }
+        }
+    }
+    printf();
+    return 
+}
+void my_free(int* p) {
+    
+}
+typedef struct my_mm {
+
+} my_mm_t;
+int g_mem[10];
+my_mm_t mms;
+
+int main() {
+    int* np1 = my_calloc(1,1);
+    int* np2 = my_calloc(1,2);
+    int* np3 = my_calloc(1,3);
+    int* np4 = my_calloc(1,4);
+    my_free(np1);
+    my_free(np4);
+    int* np5 = my_calloc(1,3);
+    return 0;
+}
